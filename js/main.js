ccf5687abf8c61add8053411b6f9d4bc95bbbaf7
Darya <artisheva2011@yandex.ru>
2021-06-07 00:58:05 +0300

    Домашняя работа №2

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+const getRandomNumber = function (minValue, MaxValue) {
+  //источник MDN https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+  let min = minValue < MaxValue ? Math.ceil(minValue) : Math.ceil(MaxValue);
+  let max = MaxValue < minValue ? Math.ceil(MaxValue) : Math.ceil(minValue);
+
+  if (min<0 && max<0) {
+    return false;
+  }
+  if (min<0) {
+    min=0;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+};
+
+getRandomNumber();
+
+//Функция для проверки максимальной длины строки
+const checkCommentLength = function (currentLine, maxLength) {
+  return currentLine.length-1<= maxLength;
+};
+
+checkCommentLength();
