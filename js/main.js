bcb33c47a72463726e1731e48a811c0326b9df68
Darya <artisheva2011@yandex.ru>
2021-06-07 20:20:40 +0300

    Поправила на const

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const getRandomNumber = function (minValue, MaxValue) {
   //источник MDN https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
   let min = minValue < MaxValue ? Math.ceil(minValue) : Math.ceil(MaxValue);
-  let max = MaxValue < minValue ? Math.ceil(MaxValue) : Math.ceil(minValue);
+  const max = MaxValue < minValue ? Math.ceil(MaxValue) : Math.ceil(minValue);
 
   if (min<0 && max<0) {
     return false;
